ef220e31ae1ccec176841853df043e8fa9995226
Anil Pujara <anilpujara125@gmail.com>
2024-09-15 21:28:33 +0545

    playwright

--- e2e/login/login.spec.ts
+++ e2e/login/login.spec.ts
@@ -1,3 +1,12 @@
-import { test } from "@playwright/test";
+require("dotenv").config();
 
-test("", async ({ page }) => {});
+import { LoginPage } from "@pages/login/login";
+import { expect, test } from "@playwright/test";
+let loginP = new LoginPage();
+
+test("Login", async ({ page }) => {
+  await page.locator(loginP.form().username).fill(process.env.USER_NAME);
+  await page.locator(loginP.form().password).fill(process.env.PASSWORD);
+  await page.locator(loginP.button().login).click();
+  await expect(page).toHaveURL("/dashboard");
+});
