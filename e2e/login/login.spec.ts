77f4ef5b96cd9f183668703ba9b9daebb1cf7ad6
Anil Pujara <anilpujara125@gmail.com>
2024-09-15 07:31:01 +0545

    playwright

--- e2e/login/login.spec.ts
+++ e2e/login/login.spec.ts
@@ -1,14 +1,3 @@
-import { LoginPageObj } from "@pageObject/loginObj/loginObj";
-import { expect, test } from "@playwright/test";
+import { test } from "@playwright/test";
 
-let loginPageObj = new LoginPageObj();
-
-test("Login", async ({ page }) => {
-  await page.goto("https://frontend.midashealthservices.com.np/");
-  await page.getByRole("heading", { name: "Welcome back" });
-  await page.locator("#username").fill("superadmin");
-  await page.locator("#password").fill("1KNWPVnOJxyO3at");
-  await page.locator("button[type='submit']").click();
-  await page.locator("div[data-cy='User Management']").click();
-  await page.url().includes("/user");
-});
+test("", async ({ page }) => {});
