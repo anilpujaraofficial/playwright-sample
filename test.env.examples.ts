0bd0064c9773749650b2546f35a085b027031821
Anil Pujara <anil.pujara@midastechnologies.com.np>
2026-06-16 10:29:26 +0545

    update

--- test.env.examples.ts
+++ test.env.examples.ts
@@ -1,15 +1,14 @@
 const env = {
   dev: {
-    url: "https://dev.midashealthservices.com.np",
-    apiUrl: "https://um.dev.midashealthservices.com.np/api/v1",
-    username: "superadmin",
-    password: "Mid@s!23##",
+    url: null,
+    apiUrl: null,
+    username: null,
+    password: null,
   },
 };
 
 //add env
 const testEnv = "dev"; //TODO
-
 export function getClientEnv(key) {
   return env[testEnv][key];
 }
